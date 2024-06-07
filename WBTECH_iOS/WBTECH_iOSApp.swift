dc861006887161b5f8b26d6454fa777ceec7a7f5
Samir Akhmadi <samirakhmadi@gmail.com>
2024-06-07 01:28:53 -0700

    Initial Commit

---
+++ WBTECH_iOS/WBTECH_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WBTECH_iOSApp.swift
+//  WBTECH_iOS
+//
+//  Created by Samir on 07.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct WBTECH_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
